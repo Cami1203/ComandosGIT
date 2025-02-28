8078b90edf26527f2b7bd4469da04c527c1b99e4
CamiloT <tr.cristiancamilo12@gmail.com>
2025-02-27 22:59:25 -0500

    cambios3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("para listar bases de datos se utiliza SHOW DATABASES;git ");
+        System.out.println("para insertar registros se untiliza el insert Into");
+        System.out.println("mas adelante veremos los conocidos TRIGGER");
 
 
     }

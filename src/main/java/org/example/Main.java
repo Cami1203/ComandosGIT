83aa869a80898dab6ce4eb593c502da30c3c19f6
CamiloT <tr.cristiancamilo12@gmail.com>
2025-02-27 21:21:29 -0500

    cambios

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("El fracaso es éxito si aprendemos de él");
+        System.out.println("El éxito no es definitivo; el fracaso no es fatal. Lo que realmente cuenta es tener valor para continuar");
+        System.out.println("Cristian Camilo Tique");
 
 
     }

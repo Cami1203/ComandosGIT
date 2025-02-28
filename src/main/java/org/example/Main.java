d3eb6e91dcf4fda5206cb3593620d40865a47ddf
CamiloT <tr.cristiancamilo12@gmail.com>
2025-02-27 22:46:44 -0500

    cambios2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("El fracaso es éxito si aprendemos de él");
-        System.out.println("El éxito no es definitivo; el fracaso no es fatal. Lo que realmente cuenta es tener valor para continuar");
-        System.out.println("Cristian Camilo Tique");
+        System.out.println("para listar bases de datos se utiliza SHOW DATABASES;git ");
 
 
     }
